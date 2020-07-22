ee88047db0fbbeb9e23600ba2d069039f41bcb6d
iam1412 <iydotaomza@gmail.com>
2020-07-22 20:40:18 +0700

    frist test

--- firebog.php
+++ firebog.php
@@ -9,9 +9,17 @@ $bebug = false;
 $url = "https://v.firebog.net/hosts/lists.php?type=all";
 $data = explode("\n", trim(file_get_contents($url)));
 _print_r($data);
+if(is_array($data)){
+    foreach($data as $single_url){
+        $single_data = explode("\n", trim(file_get_contents($single_url)));
+        _print_r($single_data);
+    break;
+    }
+}
 
 
 function _print_r($data = array()){
+    global $debug;
     if($debug){
         echo '<pre>';
         print_r($data);
