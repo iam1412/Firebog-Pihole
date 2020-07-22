a15da521d2d4b248d9577a3b02810ccf7167ac14
iam1412 <iydotaomza@gmail.com>
2020-07-22 21:54:58 +0700

    Update firebog.php

--- firebog.php
+++ firebog.php
@@ -4,6 +4,8 @@ firebog - https://firebog.net
 All type hosts - https://v.firebog.net/hosts/lists.php?type=all
 */
 
+/*
+// Show only : Not unique 
 $url = "https://v.firebog.net/hosts/lists.php?type=all";
 $data = explode("\n", trim(file_get_contents($url)));
 if(is_array($data)){
@@ -11,4 +13,22 @@ if(is_array($data)){
         echo file_get_contents($single_url) . "\n\n\n";
     }
 }
+*/
+
+// Unique
+$all = array();
+$url = "https://v.firebog.net/hosts/lists.php?type=all";
+$data = explode("\n", trim(file_get_contents($url)));
+if(is_array($data)){
+    foreach($data as $single_url){
+        $thum = explode("\n",file_get_contents($single_url));
+        $all = array_combine($all, $thum);
+    }
+}
+$all = array_unique($all);
+foreach($all as $single){
+    echo $single . "\n";
+}
+
+
 ?>
