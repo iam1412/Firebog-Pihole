6141793b38febc1d71442ca50160bd2a8d0e4d0a
iam1412 <iydotaomza@gmail.com>
2020-07-22 20:39:34 +0700

    debug frist url

--- firebog.php
+++ firebog.php
@@ -4,10 +4,19 @@ firebog - https://firebog.net
 All type hosts - https://v.firebog.net/hosts/lists.php?type=all
 */
 
+$bebug = false;
+
 $url = "https://v.firebog.net/hosts/lists.php?type=all";
-$data = explode("\n", file_get_contents($url));
-echo '<pre>';
-print_r($data);
-echo '</pre>';
+$data = explode("\n", trim(file_get_contents($url)));
+_print_r($data);
+
+
+function _print_r($data = array()){
+    if($debug){
+        echo '<pre>';
+        print_r($data);
+        echo '</pre>';
+    }
+}
 
 ?>
