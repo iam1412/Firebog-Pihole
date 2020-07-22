9b667370d3f09938cc626ab48da9be400d569290
iam1412 <iydotaomza@gmail.com>
2020-07-22 21:49:15 +0700

    Update firebog.php

--- firebog.php
+++ firebog.php
@@ -8,7 +8,7 @@ $url = "https://v.firebog.net/hosts/lists.php?type=all";
 $data = explode("\n", trim(file_get_contents($url)));
 if(is_array($data)){
     foreach($data as $single_url){
-        echo file_get_contents($single_url);
+        echo file_get_contents($single_url) . "\n\n\n";
     }
 }
 ?>
