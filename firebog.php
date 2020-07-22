65c8e548b8b233415807f490e31ddcf4b9a6a391
iam1412 <9153228+iam1412@users.noreply.github.com>
2020-07-22 21:40:37 +0700

    master

--- firebog.php
+++ firebog.php
@@ -4,30 +4,12 @@ firebog - https://firebog.net
 All type hosts - https://v.firebog.net/hosts/lists.php?type=all
 */
 
-$debug = false;
-$all_data = array();
-
 $url = "https://v.firebog.net/hosts/lists.php?type=all";
 $data = explode("\n", trim(file_get_contents($url)));
 _print_r($data);
 if(is_array($data)){
     foreach($data as $single_url){
-        $single_data = explode("\n", trim(file_get_contents($single_url)));
-        _print_r($single_data);
-        $all_data = array_merge($all_data, $single_data);
+        echo file_get_contents($single_url);
     }
 }
-
-echo count($all_data);
-
-
-function _print_r($data = array()){
-    global $debug;
-    if($debug){
-        echo '<pre>';
-        print_r($data);
-        echo '</pre>';
-    }
-}
-
 ?>
